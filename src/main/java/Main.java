af4c5f2b83ca687d89909d73454bb6492f615453
AlenaQA <alyona.trefilova@gmailcom>
2019-10-17 15:29:48 +0700

    Work with table.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+
+    static WebDriver driver;
+
     public static void main(String[] args) {
-        System.out.println("Guten Tag!");
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver_win32 (1)\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("https://www.w3schools.com/html/html_tables.asp");
+        WebElement tableElement =  driver.findElement(By.xpath("//table[@id='customers']"));
     }
 }
