5d05e4db0eb78f1dd6847d379abd519127b5b969
AlenaQA <alyona.trefilova@gmail.com>
2019-10-18 14:34:24 +0700

    Realization is done.

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -40,12 +41,30 @@ public class Table {
     public List<Map<String, WebElement>> getRowsWithColumnsByHeading() {
         List<List<WebElement>> rowsWithColumns = getRowsWithColumns();
         List<Map<String, WebElement>> rowsWithColumnsByHeadings = new ArrayList<Map<String, WebElement>>();
+        Map<String, WebElement> rowByHeadings;
+        List<WebElement> headingColumns = getHeadings();
 
+        for(List<WebElement> row : rowsWithColumns) {
+            rowByHeadings = new HashMap<String, WebElement>();
+
+            for (int i = 0; i < headingColumns.size(); i++) {
+                String heading = headingColumns.get(i).getText();
+                WebElement cell = row.get(i);
+                rowByHeadings.put(heading, cell);
+            }
+            rowsWithColumnsByHeadings.add(rowByHeadings);
+        }
+        return rowsWithColumnsByHeadings;
     }
 
     public String getValueFromCell(int rowNumber, int columnNumber) {
         List<List<WebElement>> rowsWithColumns = getRowsWithColumns();
         WebElement cell = rowsWithColumns.get(rowNumber - 1).get(columnNumber - 1);
         return cell.getText();
+    }
+
+    public String getValueFromCell(int rowNumber, String columnName) {
+        List<Map<String, WebElement>> rowsWithColumnsByHeading = getRowsWithColumnsByHeading();
+        return rowsWithColumnsByHeading.get(rowNumber - 1).get(columnName).getText();
     }
 }
